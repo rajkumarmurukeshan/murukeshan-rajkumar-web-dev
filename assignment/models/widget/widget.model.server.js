92c5d67d8f9404f4216bf50ef652576f10bb2734
Rajkumar Murukeshan <murukeshanr@ccs.neu.edu>
2016-06-19 18:27:13 -0400

    Minor Changes

--- assignment/models/widget/widget.model.server.js
+++ assignment/models/widget/widget.model.server.js
@@ -59,25 +59,7 @@ module.exports = function () {
         delete widget._id;
         return Widget.update(
             {_id: widgetId},
-            {$set: {
-                _page: widget._page,
-                type: widget.type,
-                name: widget.name,
-                text: widget.text,
-                placeholder: widget.placeholder,
-                description: widget.description,
-                url: widget.url,
-                width: widget.width,
-                height: widget.height,
-                rows: widget.rows,
-                size: widget.size,
-                class: widget.class,
-                icon: widget.icon,
-                deletable: widget.deletable,
-                formatted: widget.formatted,
-                dateCreated: widget.dateCreated,
-                widgetNumber:  widget.widgetNumber
-            }}
+            {$set: widget}
         );
     }
 
